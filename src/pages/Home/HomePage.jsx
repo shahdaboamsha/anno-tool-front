457c6c7e30af078e103c894ccc3933d0277d5ea8
shahdaboamsha <shahdmoh2002m@gmail.com>
2025-04-24 23:13:03 +0300

    Update HomePage.jsx

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -19,75 +19,42 @@ const quickActionButtonProps = {
 export default function HomePage() {
 
     const navigate = useNavigate()
-    const ref = useRef()
     const theme = useTheme()
     const isWideScreen = useMediaQuery(theme.breakpoints.up('md'))
 
     const [dialogOpenState, setDialogOpenState] = useState(false)
     const [component, setComponent] = useState(<SigninForm />)
 
-    // handle Get Started button -> open the form of the task creation
-    const getStarted = () => {
-        if (localStorage.getItem('ACCESS_TOKEN')) {
-            navigate('/dashboard/overview')
-        }
-        else {
-            setDialogOpenState(true)
-
-        }
-    }
-
+    // handle Get Started button -> open the form of the task creation or go to the dashboard
+    const getStarted = () => localStorage.getItem('ACCESS_TOKEN')? navigate('/dashboard/overview') :  setDialogOpenState(true)
+    
     return (
-        <div ref={ref}>
-            <div
-                className='text-white flex flex-centered-items flex-column-items white-text center-text home-img-container'
-                size={12}
-                style={{
-                    height: `${isWideScreen ? '90vh' : '70vh'}`,
-                    position: 'relative'
-                }}
-            >
-                <div style={{
-                    position: 'absolute',
-                    top: '0px',
-                    width: '100%'
-                }}>
+        <div>
+            <div className='text-white flex flex-centered-items flex-column-items white-text center-text home-img-container'
+                style={{ height: `${isWideScreen ? '90vh' : '70vh'}`,  position: 'relative' }} >
+               
+                <div style={{ position: 'absolute', top: '0px',  width: '100%' }}>
                     <NavigationBar />
                 </div>
 
-                <Typography fontWeight={700}
-                    variant={isWideScreen ? 'h2' : 'h4'}
-                    sx={{color: 'white'}}
-                >
+                <Typography fontWeight={700} variant={isWideScreen ? 'h2' : 'h4'} sx={{color: 'white'}} >
                     Streamline Your Data with Our Text Annotation Tool
                 </Typography>
 
-                <Typography
-                    variant={isWideScreen ? 'h6' : 'body2'}
-                    marginTop={1}
-                    sx={{color: 'white'}}
-                >
+                <Typography variant={isWideScreen ? 'h6' : 'body2'}  marginTop={1} sx={{color: 'white'}}  >
                     Easily upload your files and classify words with precision
                 </Typography>
-                <Typography
-                    variant={isWideScreen ? 'h6' : 'body2'}
-                    sx={{color: 'white'}}
-                >
+                
+                <Typography variant={isWideScreen ? 'h6' : 'body2'}  sx={{color: 'white'}} >
                     Our intuitive tool simplifies annotation, helping you organize and analyze text effortlessly. Start streamlining your data today!
                 </Typography>
+                
                 <div>
-                    <Button
-                        id="quick-action-button"
-                        variant="contained"
-                        sx={{ ...quickActionButtonProps }}
-                        onClick={getStarted}
-                        endIcon={
-                            <ArrowForwardIcon className="arrow-icon" fontSize='large' />
-                        }
-                    >
+                    <Button id="quick-action-button" variant="contained" sx={{ ...quickActionButtonProps }} onClick={getStarted}
+                        endIcon={ <ArrowForwardIcon className="arrow-icon" fontSize='large' /> }>
                         Get started
                     </Button>
-                    <QuickDialog
+                    <QuickDialog 
                         openState={dialogOpenState}
                         setOpenState={setDialogOpenState}
                         component={component} />
